09157fd4b039fcf228703d1bce74f59e012f4faa
Anthony Miller <anthony@app-order.com>
2015-07-02 16:30:54 -0700

    Added 'given, when, then'

---
+++ unit_test_-_given,_when,_then.m
@@ -0,0 +1,14 @@
+// Unit Test - Given, When, Then
+// 
+//
+// IDECodeSnippetCompletionPrefix: gwt
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 2BB3F39C-9261-44D3-A819-BD4E9CE397D8
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+    // given
+    
+    // when
+    
+    // then
